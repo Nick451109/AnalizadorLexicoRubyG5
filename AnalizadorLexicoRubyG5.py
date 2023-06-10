8218d58ea3604745d9447f190516849d21fb54ff
AndresBorbor <adborbor@espol.edu.ec>
2023-06-10 16:28:29 -0500

    Validacion Algoritmo Andrés

--- AnalizadorLexicoRubyG5.py
+++ AnalizadorLexicoRubyG5.py
@@ -1,40 +1,47 @@
 import ply.lex as lex
 
 reserved = {
-    'if': 'IF',
-    'else':'Else',
-    'print': 'PRINT',
-    'end': 'END',
-
-    'def':'DEF',
-    'break':'BREAK',
-    'class':'CLASS',
-    'while':'WHILE',
-    'for':'FOR'
+  #Andres
+  'if':'IF',
+  'else':'ElSE',
+  'print':'PRINT',
+  'end':'END',
+  #Nick
+  'def':'DEF',
+  'break':'BREAK',
+  'class':'CLASS',
+  'while':'WHILE',
+  'for':'FOR',
+  'true':'TRUE',
+  'false':'FALSE'
 }
 
 
 tokens = (
-    'GREATERTHAN',
-    'EQUALS',
-    'STRING',
-    'AND',
-    'GREATEROREQUALS', 
-
-    'ASSIGNMENT',
-    'ASSIGNINCREMENT',
-    'ASSIGNDECREMENT',
-    'LPAREN',
-    'RPAREN',
-    'NUMBER',
+  #Andres
+  'GREATERTHAN',
+  'EQUALS',
+  'STRING',
+  'AND',
+  'GREATEROREQUALS',
+  'VARIABLE',
+  #Nick
+  'ASSIGNMENT',
+  'ASSIGNINCREMENT',
+  'ASSIGNDECREMENT',
+  'LPAREN',
+  'RPAREN',
+  'NUMBER',
 ) + tuple(reserved.values())
 
+#Andres
 t_GREATERTHAN = r'>'
 t_EQUALS = r'=='
 t_GREATEROREQUALS = r'>='
-t_STRING = r'"[^".]*"'
+t_STRING =r'"[^".]*"'
 t_AND = r'&&'
 
+#Nick
 t_ASSIGNMENT = r'='
 t_ASSIGNINCREMENT = r'\+='
 t_ASSIGNDECREMENT = r'-='
@@ -42,7 +49,13 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_NUMBER = r'\d+(\.\d)?'
 
+t_ignore = ' \t'
 
+#Andres
+def t_VARIABLE(t):
+  r'(\$|@)?[a-zA-Z]+'
+  t.type = reserved.get(t.value, 'VARIABLE')
+  return t
 
 def t_newLine(t):
     r'\n+'
@@ -55,22 +68,24 @@ def t_error(t):
   print("Componente léxico no reconocido '%s'" % t.value[0])
   t.lexer.skip(1)
 
-algoritmo = '''
-    edad = 18
-    licencia = true
 
-    if(edad>=18 && licencia)
-        print "Puede manejar"
-    else
-        print "No puede manejar"
-    end
-'''
 lexer = lex.lex()
+#Andres
+algoritmoAndres = '''edad = 18
+licencia = true
 
-lexer.input(algoritmo)
+if(edad>=18 && licencia == true)
+    print "Puede manejar"
+else
+    print "No puede manejar"
+end
+'''
+
+
+lexer.input(algoritmoAndres)
 # Tokenizador
 while True:
   tok = lexer.token()
   if not tok:
-    break  #Rompe
+    break
   print(tok)
